dd2e16e9439983524f8e15c4ccf8d44e2ee45b4c
Niek7110 <niek.7110@gmail.com>
2024-11-10 02:43:39 +0900

    2-2完了;

--- flutter_tutorials/lib/main.dart
+++ flutter_tutorials/lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello Flutter!'),
+          title: Text('Hello Flutter!'),
         ),
-        body: const Text(
-          'Hello Flutter World!!',
+        body: Text(
+          'Hello Flutter World',
           style: TextStyle(fontSize: 32.0),
         ),
       ),
